5deb2b5307770fc2db47342b411ea06fd8d74fbe
Marcos Wade <marcoswade@gmail.com>
2017-10-23 05:55:31 -0400

    change test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,22 +4,13 @@ import Home from './pages/home';
 import {shallow} from 'enzyme';
 
 function setup(){
-  const props = {
-    openEditName: jest.fn()
-  };
-
-  const enzymeWrapper = shallow(<App {...props} ><Home /></App>);
-
-  return {
-    props,
-    enzymeWrapper
-  };
+  return shallow(<App ><Home /></App>);
 }
 
 describe('components', ()=>{
   describe('App', () =>{
-    it('should render without crashing', () =>{
-      const {enzymeWrapper} = setup();
+    it('should render a div with class "App"', () =>{
+      const enzymeWrapper = setup();
 
       expect(enzymeWrapper.find('div').hasClass('App')).toBe(true);
     });
